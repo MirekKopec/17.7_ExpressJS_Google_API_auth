6e5a23e8c96f6673a0b2ef56ef5796bef1180e3a
MirekKopec <mkopec95@gmail.com>
2018-01-25 21:34:27 +0100

    Google APi project first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '653870954894-5ck5tca9hrb5fbt7erotln6toaeriohm.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'rtDKgy02uGsukBRvrBwJOlnE',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
